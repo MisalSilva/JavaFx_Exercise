8317f2239dfa9f51dec01339daa3b72931b87ab9
MisalSilva <misal.20230183@iit.ac.lk>
2024-12-13 14:33:50 +0530

    Update

--- src/main/java/com/example/javafx/HelloController.java
+++ src/main/java/com/example/javafx/HelloController.java
@@ -11,4 +11,8 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    public void buttonClick(){
+        System.out.println("Button clicked");
+    }
 }
